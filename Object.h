33bd645bab34fc46fab463b35fa592d3b3afde61
trung nguyen <trungnv@Trungs-MacBook-Pro-2.local>
2013-12-16 16:55:49 +0700

    commit

---
+++ Object.h
@@ -0,0 +1,13 @@
+//
+//  Object.h
+//  Challenge
+//
+//  Created by Trung Nguyen Van on 12/16/13.
+//  Copyright (c) 2013 Evil Genius Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Object : NSObject
+
+@end
---
+++ Object.m
@@ -0,0 +1,13 @@
+//
+//  Object.m
+//  Challenge
+//
+//  Created by Trung Nguyen Van on 12/16/13.
+//  Copyright (c) 2013 Evil Genius Technologies. All rights reserved.
+//
+
+#import "Object.h"
+
+@implementation Object
+
+@end
